213d3b482be4bfe5a990b56ff729c1594b0a7042
Rakastajien kilta <rakastajienkilta@rakastajienkilta.fi>
2025-09-02 11:36:27 +0300

    Change config to be more translatable

--- config.php
+++ config.php
@@ -2,150 +2,163 @@
 const OutputFile = "data/form_data.json";
 const SecretPassword = "porkkana";
 
-$formTitle = "Ilmoittaudu";
-$intro = <<<END
+$formTitle = ['fi' => "Ilmoittaudu", 'en' => 'Sign up'];
+$intro = [
+'fi' => <<<END
 <p>Lorem ipsum. Tervetuloa ja niin edelleen. Täytä kentät</p>
-END;
+END,
+'en' => <<<END
+<p>Lorem ipsum. Tervetuloa ja niin edelleen. Täytä kentät</p>
+END
+];
 
 $interest_choices = [
-  "Kyllä kiitos!",
-  "Riippuu ihmisestä",
-  "Ehkä",
-  "Ei kiitos",
+  ['fi' => "Kyllä kiitos!", 'en' => "Kyllä kiitos!"],
+  ['fi' => "Riippuu ihmisestä", 'en' => "Riippuu ihmisestä"],
+  ['fi' => "Ehkä", 'en' => "Ehkä"],
+  ['fi' => "Ei kiitos", 'en' => "Ei kiitos"],
 ];
 
 $fields = [
-  [ "subtitle" => "Henkilötiedot" ],
+  [ "subtitle" => ['fi' => "Henkilötiedot", 'en' => "Henkilötiedot"] ],
   [
     "id" => "givenname",
-    "name" => "Kutsumanimesi",
+    "name" => ['fi' => "Kutsumanimesi", 'en' => "Kutsumanimesi"],
     "type" => "text",
     "required" => true,
     "class" => "half-width",
   ],
   [
     "id" => "surname",
-    "name" => "Sukunimi",
+    "name" => ['fi' => "Sukunimi", 'en' => "Sukunimi"],
     "type" => "text",
     "required" => true,
     "class" => "half-width",
   ],
   [
-    "info" => "Pyydämme nimeäsi avoimuuden hengessä. Me järjestämme juhlat avoimesti omilla nimillämme ja uskomme tämän luottamuksen olevan tärkeä osa orgioita."
+    "info" => ['fi' => "Pyydämme nimeäsi avoimuuden hengessä. Me järjestämme juhlat avoimesti omilla nimillämme ja uskomme tämän luottamuksen olevan tärkeä osa orgioita.", 'en' => "Pyydämme nimeäsi avoimuuden hengessä. Me järjestämme juhlat avoimesti omilla nimillämme ja uskomme tämän luottamuksen olevan tärkeä osa orgioita."]
   ],
   [
     "id" => "phone",
-    "name" => "Puhelinnumero",
-    "info" => "Soitamme sinulle vain tiedottaaksemme äkillisistä muutoksista.",
+    "name" => ['fi' => "Puhelinnumero", 'en' => "Puhelinnumero"],
+    "info" => ['fi' => "Soitamme sinulle vain tiedottaaksemme äkillisistä muutoksista.", 'en' => "Soitamme sinulle vain tiedottaaksemme äkillisistä muutoksista."],
     "type" => "text",
     "required" => false,
     "class" => "half-width",
   ],
   [
     "id" => "birthyear",
-    "name" => "Syntymävuotesi",
-    "info" => "Käytämme tätä tietoa osallistujien ikäjakauman selvittämiseen.",
+    "name" => ['fi' => "Syntymävuotesi", 'en' => "Syntymävuotesi"],
+    "info" => ['fi' => "Käytämme tätä tietoa osallistujien ikäjakauman selvittämiseen.", 'en' => "Käytämme tätä tietoa osallistujien ikäjakauman selvittämiseen."],
     "type" => "number",
     "required" => false,
     "class" => "half-width",
   ],
   [
     "id" => "languagepreference",
-    "name" => "Mitä kieltä suosit?",
+    "name" => ['fi' => "Mitä kieltä suosit?", 'en' => "Mitä kieltä suosit?"],
     "type" => "radio",
-    "choices" => ["suomi", "ruotsi", "englanti"],
+    "choices" => [
+      ['fi' => "suomi", 'en' => "suomi"],
+      ['fi' => "ruotsi", 'en' => "ruotsi"],
+      ['fi' => "englanti", 'en' => "englanti"]
+    ],
     "required" => true,
   ],
   [
     "id" => "allegies",
-    "name" => "Terveydelle vaaralliset allergiat, joista järjestäjien on syytä tietää",
+    "name" => ['fi' => "Terveydelle vaaralliset allergiat, joista järjestäjien on syytä tietää", 'en' => "Terveydelle vaaralliset allergiat, joista järjestäjien on syytä tietää"],
     "type" => "textarea",
     "required" => false,
   ],
   [
     "id" => "sexorgans",
-    "name" => "Millaiset sukuelimet sinulla on?",
-    "info" => "Yhdessä kiinnostustesi kanssa hyödynnämme tätä tietoa mahdollisimman tasapainoisen osallistujaryhmän kokoamisessa.",
+    "name" => ['fi' => "Millaiset sukuelimet sinulla on?", 'en' => "Millaiset sukuelimet sinulla on?"],
+    "info" => ['fi' => "Yhdessä kiinnostustesi kanssa hyödynnämme tätä tietoa mahdollisimman tasapainoisen osallistujaryhmän kokoamisessa.", 'en' => "Yhdessä kiinnostustesi kanssa hyödynnämme tätä tietoa mahdollisimman tasapainoisen osallistujaryhmän kokoamisessa."],
     "type" => "radio",
-    "choices" => ["naaras", "uros", "muu"],
+    "choices" => [
+      ['fi' => "naaras", 'en' => "naaras"],
+      ['fi' => "uros", 'en' => "uros"],
+      ['fi' => "muu", 'en' => "muu"]
+    ],
     "required" => true,
   ],
-  [ "subtitle" => "Kiinnostukset" ],
+  [ "subtitle" => ['fi' => "Kiinnostukset", 'en' => "Kiinnostukset"] ],
   [
-    "info" => "Jos rakastelukumppanien sukupuolielinten tyypillä on sinulle jotakin merkitystä, kerro siitä meille."
+    "info" => ['fi' => "Jos rakastelukumppanien sukupuolielinten tyypillä on sinulle jotakin merkitystä, kerro siitä meille.", 'en' => "Jos rakastelukumppanien sukupuolielinten tyypillä on sinulle jotakin merkitystä, kerro siitä meille."]
   ],
   [
     "id" => "interest-in-females",
-    "name" => "Kuinka kiinnostunut olet lempimään sellaisten ihmisten kanssa, joilla on naaraan sukuelimet?",
+    "name" => ['fi' => "Kuinka kiinnostunut olet lempimään sellaisten ihmisten kanssa, joilla on naaraan sukuelimet?", 'en' => "Kuinka kiinnostunut olet lempimään sellaisten ihmisten kanssa, joilla on naaraan sukuelimet?"],
     "type" => "radio",
     "choices" => $interest_choices,
     "required" => true,
   ],
   [
     "id" => "interest-in-males",
-    "name" => "Kuinka kiinnostunut olet lempimään sellaisten ihmisten kanssa, joilla on uroksen sukuelimet?",
+    "name" => ['fi' => "Kuinka kiinnostunut olet lempimään sellaisten ihmisten kanssa, joilla on uroksen sukuelimet?", 'en' => "Kuinka kiinnostunut olet lempimään sellaisten ihmisten kanssa, joilla on uroksen sukuelimet?"],
     "type" => "radio",
     "choices" => $interest_choices,
     "required" => true,
   ],
   [
-    "info" => "Minkä ikäisistä olet kiinnostunut?"
+    "info" => ['fi' => "Minkä ikäisistä olet kiinnostunut?", 'en' => "Minkä ikäisistä olet kiinnostunut?"]
   ],
   [
     "id" => "min-age",
-    "name" => "Vähimmäisikä",
+    "name" => ['fi' => "Vähimmäisikä", 'en' => "Vähimmäisikä"],
     "type" => "number",
     "required" => false,
     "class" => "half-width",
   ],
   [
     "id" => "max-age",
-    "name" => "Enimmäisikä",
+    "name" => ['fi' => "Enimmäisikä", 'en' => "Enimmäisikä"],
     "type" => "number",
     "required" => false,
     "class" => "half-width",
   ],
-  [ "subtitle" => "Avecit ja epätoivotut henkilöt" ],
+  [ "subtitle" => ['fi' => "Avecit ja epätoivotut henkilöt", 'en' => "Avecit ja epätoivotut henkilöt"] ],
   [
     "id" => "avecs",
-    "name" => "Mahdollisten avecciesi nimet ja sähköpostiosoitteet",
+    "name" => ['fi' => "Mahdollisten avecciesi nimet ja sähköpostiosoitteet", 'en' => "Mahdollisten avecciesi nimet ja sähköpostiosoitteet"],
     "type" => "textarea",
     "required" => false,
   ],
   [
     "id" => "nongratas",
-    "name" => "Niiden henkilöiden nimet, joiden kanssa et koe voivasi osallistua orgioihin ja joiden uskot voivan osallistua näihin juhliin",
-    "info" => "Kerromme sinulle onko joku heistä ilmoittautunut mukaan, mutta emme tietenkään kerro kuka tai ketkä. Voit sitten perua oman osallistumisesi, jos niin tahdot. Tämä on toinen syy nimienne kysymiselle.",
+    "name" => ['fi' => "Niiden henkilöiden nimet, joiden kanssa et koe voivasi osallistua orgioihin ja joiden uskot voivan osallistua näihin juhliin", 'en' => "Niiden henkilöiden nimet, joiden kanssa et koe voivasi osallistua orgioihin ja joiden uskot voivan osallistua näihin juhliin"],
+    "info" => ['fi' => "Kerromme sinulle onko joku heistä ilmoittautunut mukaan, mutta emme tietenkään kerro kuka tai ketkä. Voit sitten perua oman osallistumisesi, jos niin tahdot. Tämä on toinen syy nimienne kysymiselle.", 'en' => "Kerromme sinulle onko joku heistä ilmoittautunut mukaan, mutta emme tietenkään kerro kuka tai ketkä. Voit sitten perua oman osallistumisesi, jos niin tahdot. Tämä on toinen syy nimienne kysymiselle."],
     "type" => "textarea",
     "required" => false,
   ],
   [ "subtitle" => "Muuta" ],
   [
     "id" => "info",
-    "name" => "Muut asiat, jotka haluaisit meidän tietävän",
+    "name" => ['fi' => "Muut asiat, jotka haluaisit meidän tietävän", 'en' => "Muut asiat, jotka haluaisit meidän tietävän"],
     "type" => "textarea",
     "required" => false,
   ],
   [
     "id" => "staysfornight",
-    "name" => "Oletko jäämässä yöksi?",
+    "name" => ['fi' => "Oletko jäämässä yöksi?", 'en' => "Oletko jäämässä yöksi?"],
     "type" => "radio",
     "choices" => ["Kyllä!", "En", "En tiedä vielä"],
-    "info" => "Varaamme aamupalatarpeita sen mukaan, kuinka moni on syömässä.",
+    "info" => ['fi' => "Varaamme aamupalatarpeita sen mukaan, kuinka moni on syömässä.", 'en' => "Varaamme aamupalatarpeita sen mukaan, kuinka moni on syömässä."],
     "required" => true,
   ],
   [
     "id" => "codeword",
-    "name" => "Kirjoita tähän koodisana ".SecretPassword,
+    "name" => ['fi' => "Kirjoita tähän koodisana ".SecretPassword, 'en' => "Kirjoita tähän koodisana ".SecretPassword],
     "type" => "riddle",
     "required" => true,
     "answer" => SecretPassword,
-    "errormessage" => SecretPassword . "! Ei selleri!",
+    "errormessage" => ['fi' => SecretPassword . "! Ei selleri!", 'en' => '??'],
   ],
   [
     "id" => "consenttosave",
-    "name" => "Suostutko siihen että henkilötietosi tallennetaan rakastajien killan osallistujarekisteriin",
-    "checkboxlabel" => "Kyllä",
+    "name" => ['fi' => "Suostutko siihen että henkilötietosi tallennetaan rakastajien killan osallistujarekisteriin", 'en' => "Suostutko siihen että henkilötietosi tallennetaan rakastajien killan osallistujarekisteriin"],
+    "checkboxlabel" => ['fi' => "Kyllä", 'en' => "Kyllä"],
     "type" => "checkbox",
     "required" => true,
   ]
