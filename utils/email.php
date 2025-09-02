5ee07a1dc10360486791cfb11ae5512170b9fb07
Rakastajien kilta <rakastajienkilta@rakastajienkilta.fi>
2025-09-02 13:16:24 +0300

    HTML email

--- utils/email.php
+++ utils/email.php
@@ -1,38 +1,19 @@
 <?php
 require_once('config.php');
 require_once('./utils/translate.php');
+require_once('./utils/data_summary.php');
 
 $title = translate(['fi' => 'Ilmoittautumisesi', "en" => "Your sign up"]);
-const No = ["fi" => "Ei", "en" => "No"];
 
 ob_start();
 
-echo $title . "\n\n";
-foreach($fields as $field):
-?>
-
-<?php if ($field['subtitle']): ?>
-=== <?= translate($field['subtitle']) ?> ===
-
-<?php endif; ?>
-<?php if (isset($field['id'])): ?>
-<?= translate($field['name']); ?>:
-<?php
-$value = $_POST[$field['id']];
-switch($field['type']) {
-  default:
-    if (trim($value) == '') echo '-';
-    else echo $value;
-    break;
-  case 'checkbox':
-    echo translate($value == "true" ? $field['checkboxlabel'] : No);
-    break;
-}
-?>
-
-<?php endif; ?>
-<?php endforeach;
+echo '<h1'>$title . "</h1>\n";
+renderFieldSummary(true);
 
 $body = ob_get_clean();
 
-mail(trim($_POST['email']), $title, $body, 'From: no-reply@rakastajienkilta.fi');
+$headers[] = 'MIME-Version: 1.0';
+$headers[] = 'From: no-reply@rakastajienkilta.fi';
+$headers[] = 'Content-type: text/html; charset=utf-8';
+
+mail(trim($_POST['email']), $title, $body, implode("\r\n", $headers));
