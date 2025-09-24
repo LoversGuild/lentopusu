15c9b95abe58eeb51a68d9e97a828f98222215ed
Aura Kelloniemi <kaura.dev@sange.fi>
2025-09-24 22:20:08 +0300

    Don't print riddle fields in summaries

--- views/summary/data.php
+++ views/summary/data.php
@@ -3,23 +3,23 @@ const No =  ["fi" => "Ei", "en" => "No"];
 
 function renderFieldSummary($email) {
   global $fields;
-  foreach($fields as $field):?>
+  foreach($fields as $field): ?>
     <?php if (isset($field['subtitle'])): ?>
     <h2><?= translate($field['subtitle']) ?></h2>
     <?php endif; ?>
-    <?php if (isset($field['id'])): ?>
+    <?php if (isset($field['id']) && ($field['type'] ?? '') != 'riddle'): ?>
     <div <?php if (isset($field['class'])): ?> class="<?= $field['class'] ?>"<?php endif; ?>>
       <?= translate($field['name']); ?>:
       <div className="summary-value">
         <?php require('./views/summary/field.php'); ?>
       </div>
-      <?php if (!$email): ?>
-      <input
-        type="hidden"
-        name="<?= $field['id'] ?>"
-        value="<?= htmlspecialchars($_POST[$field['id']], ENT_QUOTES, 'UTF-8') ?>" />
-      <?php endif; ?>
     </div>
+    <?php endif; ?>
+    <?php if (!$email && isset($field['id'])): ?>
+    <input
+      type="hidden"
+      name="<?= $field['id'] ?>"
+      value="<?= htmlspecialchars($_POST[$field['id']], ENT_QUOTES, 'UTF-8') ?>" />
     <?php endif; ?>
   <?php endforeach;
 }
