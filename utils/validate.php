90c114b0e9cd3f855f5a0d43eebc5e6cbda45fec
Aura Kelloniemi <kaura.dev@sange.fi>
2025-09-24 22:15:15 +0300

    Validate number fields

--- utils/validate.php
+++ utils/validate.php
@@ -2,9 +2,10 @@
 require_once('./utils/translate.php');
 
 const FieldIsRequired = ["fi" => "Kenttä on pakollinen", "en" => "The field is required"];
+const FieldNeedsToBeANumber = ["fi" => "Kentän pitää sisältää numero", "en" => "The field needs to contain a number"];
 const DefaultRiddleError = [
   "fi" => "Väärä vastaus. Luethan ilmoittautumisohjeet tarkemmin!",
-  "en" => "Wrong answer. Please read the instructions more carefully!"
+  "en" => "Wrong answer. Please read the signup instructions more carefully!"
 ];
 
 function validate($fields) {
@@ -17,13 +18,17 @@ function validate($fields) {
     if (!isset($field['id'])) continue;
     $id = $field['id'];
 
-    $value = $_POST[$id] ?? '';
+    $value = trim($_POST[$id] ?? '');
     
 
-    if ($field['required'] && trim($value) == '') {
+    if ($field['required'] && $value == '') {
       $errors[$id] = translate(FieldIsRequired);
     }
-    if ($field['type'] == 'riddle' && strcasecmp(trim($value), $field['answer']) != 0) {
+    if ($field['type'] == 'number'
+        && preg_match('/^\s*\d+\s*$/', $value) !== 1
+        && ($field['required'] || $value == '')) {
+      $errors[$id] = translate(FieldNeedsToBeANumber);
+    } else if ($field['type'] == 'riddle' && strcasecmp(trim($value), $field['answer']) != 0) {
       $errors[$id] = translate($field['errormessage'] ?? DefaultRiddleError);
     }
   }
